3d486ea08ab7bd7ffde28d20c713f618a66f8aa5
Janith Bandara <janith3000@gmail.com>
2016-02-19 23:31:43 +0530

    Added changes.

---
+++ src/main/java/org/audit4j/microservice/HTTPServer.java
@@ -0,0 +1,38 @@
+package org.audit4j.microservice;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Vertx;
+import io.vertx.core.http.HttpServerResponse;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.web.Router;
+import io.vertx.ext.web.RoutingContext;
+import io.vertx.ext.web.handler.BodyHandler;
+import io.vertx.ext.web.handler.StaticHandler;
+
+public class HTTPServer extends AbstractVerticle {
+
+    @Override
+    public void start() {
+        Router router = Router.router(vertx);
+
+        router.route().handler(BodyHandler.create());
+        router.get("/health").handler(this::handlerHealth);
+        router.post("/rest/event").handler(new RESTAuditEventHandler()::handleEvent);
+        router.get("/manage/client").handler(new RESTAuditEventHandler()::handleEvent);
+
+        router.route("/*").handler(StaticHandler.create());
+
+        vertx.createHttpServer().requestHandler(router::accept).listen(8080);
+    }
+
+    private void handlerHealth(RoutingContext routingContext) {
+        HttpServerResponse response = routingContext.response();
+        response.putHeader("content-type", "application/json")
+                .end(new JsonObject().put("status", "running").encodePrettily());
+    }
+
+    public static void main(String[] args) {
+        Vertx vertx = Vertx.vertx();
+        vertx.deployVerticle(new HTTPServer());
+    }
+}
--- src/main/java/org/audit4j/microservice/RESTAuditEventHandler.java
+++ src/main/java/org/audit4j/microservice/RESTAuditEventHandler.java
@@ -3,18 +3,16 @@ package org.audit4j.microservice;
 import org.audit4j.core.dto.AuditEvent;
 import org.audit4j.microservice.transport.Ack;
 
-import io.vertx.core.Handler;
 import io.vertx.core.http.HttpServerResponse;
 import io.vertx.core.json.Json;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.RoutingContext;
 
-public class RESTAuditEventHandler implements Handler<RoutingContext> {
+public class RESTAuditEventHandler {
 
 	private EventReceiver reciever = new EventReceiverImpl();
 
-	@Override
-	public void handle(RoutingContext routingContext) {
+	public void handleEvent(RoutingContext routingContext) {
 		JsonObject jsonEvent = routingContext.getBodyAsJson();
 		AuditEvent event = Json.decodeValue(jsonEvent.encode(), AuditEvent.class);
 		reciever.receive(event);
